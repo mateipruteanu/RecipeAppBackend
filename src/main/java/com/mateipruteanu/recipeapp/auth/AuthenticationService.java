c1712f58eb1a00891d9884a5e7041d430c812448
mateipruteanu <mateipruteanu1@gmail.com>
2023-08-19 13:15:58 +0300

    implemented AuthController but with placeholder Service

---
+++ src/main/java/com/mateipruteanu/recipeapp/auth/AuthenticationService.java
@@ -0,0 +1,17 @@
+package com.mateipruteanu.recipeapp.auth;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class AuthenticationService {
+
+    public AuthenticationResponse register(RegistrationRequest request) {
+        return null;
+    }
+
+    public AuthenticationResponse login(LoginRequest request) {
+        return null;
+    }
+}

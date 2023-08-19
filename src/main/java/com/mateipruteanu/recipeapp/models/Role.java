8cf3335fe83078da4391247f3d34a2ef50e9444d
mateipruteanu <mateipruteanu1@gmail.com>
2023-08-19 11:02:31 +0300

    User now implements UserDetails from SpringSecurity

---
+++ src/main/java/com/mateipruteanu/recipeapp/models/Role.java
@@ -0,0 +1,6 @@
+package com.mateipruteanu.recipeapp.models;
+
+public enum Role {
+    USER,
+    ADMIN
+}
--- src/main/java/com/mateipruteanu/recipeapp/models/User.java
+++ src/main/java/com/mateipruteanu/recipeapp/models/User.java
@@ -1,30 +1,30 @@
 package com.mateipruteanu.recipeapp.models;
 
 import jakarta.persistence.*;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
 
 import java.util.ArrayList;
+import java.util.Collection;
 import java.util.List;
 
 @Entity
-public class User {
+public class User implements UserDetails {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long id;
-
     @Column(unique = true)
     private String username;
-
     @Column
     private String password;
-
     @OneToMany
     @JoinTable(
             name = "user_added_recipes",
             joinColumns = @JoinColumn(name = "user_id"),
             inverseJoinColumns = @JoinColumn(name = "recipe_id"))
     private List<Recipe> addedRecipes = new ArrayList<>();
-
     @OneToMany
     @JoinTable(
             name = "user_favorite_recipes",
@@ -32,11 +32,37 @@ public class User {
             inverseJoinColumns = @JoinColumn(name = "recipe_id"))
     private List<Recipe> favoriteRecipes = new ArrayList<>();
 
+    @Enumerated(EnumType.STRING)
+    private Role role;
+
+//    Constructors
     public User() {
         username = "";
         password = "";
     }
 
+    public User(String username, String password) {
+        this.username = username;
+        this.password = password;
+    }
+
+    public User(long id, String username, String password, List<Recipe> addedRecipes, List<Recipe> favoriteRecipes) {
+        this.id = id;
+        this.username = username;
+        this.password = password;
+        this.addedRecipes = addedRecipes;
+        this.favoriteRecipes = favoriteRecipes;
+    }
+
+    public User(String username, String password, List<Recipe> addedRecipes, List<Recipe> favoriteRecipes) {
+        this.username = username;
+        this.password = password;
+        this.addedRecipes = addedRecipes;
+        this.favoriteRecipes = favoriteRecipes;
+    }
+
+
+//    Getters and Setters
     public List<Recipe> getFavoriteRecipes() {
         return favoriteRecipes;
     }
@@ -53,10 +79,6 @@ public class User {
         this.addedRecipes = addedRecipes;
     }
 
-    public User(String username, String password) {
-        this.username = username;
-        this.password = password;
-    }
     public long getId() {
         return id;
     }
@@ -79,5 +101,31 @@ public class User {
 
     public void setPassword(String password) {
         this.password = password;
+    }
+
+//    User Details specific functions
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return List.of(new SimpleGrantedAuthority(role.name()));
     }
 }

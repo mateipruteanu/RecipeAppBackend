3ebbb2b9f70b01f528e67f3e5ca6aab19efb1345
mateipruteanu <mateipruteanu1@gmail.com>
2023-08-19 14:51:53 +0300

    added a secure and unsecure mapping for testing
    they work as expected

--- src/main/java/com/mateipruteanu/recipeapp/controllers/ApiController.java
+++ src/main/java/com/mateipruteanu/recipeapp/controllers/ApiController.java
@@ -3,9 +3,20 @@ package com.mateipruteanu.recipeapp.controllers;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
+@RequestMapping("/api")
 public class ApiController {
-    @GetMapping("/api")
+    @GetMapping("/")
     public String getInfo() {
         return "Restful API for a recipe app";
+    }
+
+    @GetMapping("/secure")
+    public String getSecureInfo() {
+        return "This is a secure endpoint";
+    }
+
+    @GetMapping("/auth/notSecure")
+    public String getNotSecureInfo() {
+        return "This is not a secure endpoint";
     }
 }

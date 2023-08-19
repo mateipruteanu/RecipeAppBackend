43609683322b507023aed78bcb61a14901dede0d
mateipruteanu <mateipruteanu1@gmail.com>
2023-08-19 12:09:51 +0300

    implemented JwtService class

---
+++ src/main/java/com/mateipruteanu/recipeapp/config/EncryptionKeyGenerator.java
@@ -0,0 +1,26 @@
+package com.mateipruteanu.recipeapp.config;
+import java.security.SecureRandom;
+
+public class EncryptionKeyGenerator {
+
+    public static void main(String[] args) {
+        SecureRandom secureRandom = new SecureRandom();
+        byte[] key = new byte[32]; // 256 bits
+        secureRandom.nextBytes(key);
+
+        String hexKey = bytesToHex(key);
+        System.out.println("Generated Key: " + hexKey);
+    }
+
+    private static final char[] HEX_ARRAY = "0123456789ABCDEF".toCharArray();
+
+    public static String bytesToHex(byte[] bytes) {
+        char[] hexChars = new char[bytes.length * 2];
+        for (int i = 0; i < bytes.length; i++) {
+            int value = bytes[i] & 0xFF;
+            hexChars[i * 2] = HEX_ARRAY[value >>> 4];
+            hexChars[i * 2 + 1] = HEX_ARRAY[value & 0x0F];
+        }
+        return new String(hexChars);
+    }
+}

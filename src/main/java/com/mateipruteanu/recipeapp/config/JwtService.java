fd1ad8c652de729a9bfc42dc3a13ae1c58594150
mateipruteanu <mateipruteanu1@gmail.com>
2023-08-28 16:49:44 +0300

    added userID to JWT
    to help with integration with the frontend

--- src/main/java/com/mateipruteanu/recipeapp/config/JwtService.java
+++ src/main/java/com/mateipruteanu/recipeapp/config/JwtService.java
@@ -1,11 +1,14 @@
 package com.mateipruteanu.recipeapp.config;
 
+import com.mateipruteanu.recipeapp.repositories.UserRepository;
 import io.github.cdimascio.dotenv.Dotenv;
 import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.MalformedJwtException;
 import io.jsonwebtoken.io.Decoders;
 import io.jsonwebtoken.security.Keys;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Service;
 
@@ -15,7 +18,8 @@ import java.util.function.Function;
 
 @Service
 public class JwtService {
-
+    @Autowired
+    private UserRepository userRepository;
     Dotenv dotenv = Dotenv.load();
     private final String secretKey = dotenv.get("SECRET_KEY");
 
@@ -57,10 +61,14 @@ public class JwtService {
     }
 
     public String generateToken(Map<String, Object> extraClaims, UserDetails userDetails) {
+        long id = -1;
+        if(userRepository.findByUsername(userDetails.getUsername()).isPresent())
+            id = userRepository.findByUsername(userDetails.getUsername()).get().getId();
         System.out.println("JWTService: Generating token that is issued at " + new java.util.Date(System.currentTimeMillis()) + " and expires at " + new java.util.Date(System.currentTimeMillis() + 1000 * 60 * 60 * 24));
         return Jwts.builder()
                 .setClaims(extraClaims)
                 .setSubject(userDetails.getUsername())
+                .claim("id", id)
                 .setIssuedAt(new java.util.Date(System.currentTimeMillis()))
                 .setExpiration(new java.util.Date(System.currentTimeMillis() + 1000 * 60 * 60 * 24)) // 24 hours
                 .signWith(getSignInKey(), io.jsonwebtoken.SignatureAlgorithm.HS256)
@@ -84,5 +92,4 @@ public class JwtService {
         byte[] keyBytes = Decoders.BASE64.decode(secretKey);
         return Keys.hmacShaKeyFor(keyBytes);
     }
-
 }
